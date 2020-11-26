cd9d1286da63bf7d0fb54e3f15041992f662d409
zjjaxx <2495893564@qq.com>
2020-11-26 15:17:11 +0800

    router

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
-
 const _koa = require("./lib/koa")
-
+const Router = require('./lib/router')
 const server = new _koa()
-const delay=()=>new Promise((resolve,reject)=>setTimeout(()=>{resolve(0)},2000))
-server.use(async (context,next)=>{
-    if(context.request.url=="/"){
-        await next()
-    }
-})
-server.use(async (context,next)  => {
-    context.body = "1"
-    await delay()
-    console.log("1 before")
-    await next()
-    context.body += "5"
-    console.log("1 end")
-})
-server.use(async (context,next)  => {
-    context.body += "2"
-    await delay()
-    console.log("2 before")
-    await next()
-    context.body += "4"
-    console.log("2 end")
-})
-server.use((context,next)  => {
-    context.body += "3"
-    console.log("3 end")
-})
-server.listen(3000, () => {
-    console.log("koa start")
-})
+const router = new Router();
+
+router.get('/index', async (ctx, next) => { ctx.body = 'index page'; });
+router.get('/post', async (ctx, next) => { ctx.body = 'post page'; });
+router.get('/list', async (ctx, next) => { ctx.body = 'list page'; });
+router.post('/index', async (ctx, next) => { ctx.body = 'post page'; });
+
+server.use(router.routesMiddleware());
+
+server.listen(3000)
