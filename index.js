2a7a10804d65cb694a2bd749d11980e66972038c
zjjaxx <2495893564@qq.com>
2020-11-26 13:53:29 +0800

    koa

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+
 const _koa = require("./lib/koa")
 
 const server = new _koa()
 const delay=()=>new Promise((resolve,reject)=>setTimeout(()=>{resolve(0)},2000))
 server.use(async (context,next)=>{
     if(context.request.url=="/"){
-        next()
+        await next()
     }
 })
 server.use(async (context,next)  => {
@@ -12,17 +13,19 @@ server.use(async (context,next)  => {
     await delay()
     console.log("1 before")
     await next()
+    context.body += "5"
     console.log("1 end")
 })
 server.use(async (context,next)  => {
-    context.body = "2"
+    context.body += "2"
     await delay()
     console.log("2 before")
     await next()
+    context.body += "4"
     console.log("2 end")
 })
 server.use((context,next)  => {
-    context.body = "3"
+    context.body += "3"
     console.log("3 end")
 })
 server.listen(3000, () => {
